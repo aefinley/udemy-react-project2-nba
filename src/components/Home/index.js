ec3f190cd802e2929f074997c9d520d9c80e7875
allison finley <allisonfinley42@gmail.com>
2019-08-29 12:27:56 -0500

    Added blocks to home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,6 +5,7 @@ import { URL_HOME } from '../Utils/paths';
 
 import SliderWidget from '../Utils/Slider';
 import Subscriptions from '../Utils/Subscribe';
+import Blocks from './Blocks';
 
 class Home extends Component {
 
@@ -15,17 +16,17 @@ class Home extends Component {
     componentDidMount() {
         axios.get(URL_HOME).then((response) => {
             console.log(response.data);
-            this.setState({
-                home: response.data
-            })
+            this.setState({ home: response.data })
         })
     }
 
     render() {
+        const home = this.state.home;
         return (
             <>
-                <SliderWidget slides={this.state.home.slider}/>
+                <SliderWidget slides={home.slider}/>
                 <Subscriptions />
+                <Blocks blocks={home.blocks}/>
             </>
         )
     }
